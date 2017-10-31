a9f118989d1be99b431368996d7b04ef285d8f2a
lhenrique <lucas@superaremedia.com.br>
2017-10-31 19:04:40 -0200

    Updating project at end of the day

---
+++ app/src/containers/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './Home.scss'
+
+class Home extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<a href="#">Toma</a>
+		)
+	}
+}
+
+export default Home
--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { Route } from 'react-router'
+import { ConnectedRouter } from 'react-router-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Home from './containers/Home'
+import cfg from './store'
+
+import './index.scss'
+
+const store = cfg.configStore()
+
+ReactDOM.render(
+	<div className="main-content">
+		<nav className="navbar navbar-dark navbar-expand-lg navbar-light bg-primary">
+			<a className="navbar-brand" href="#">Navbar</a>
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<div className="collapse navbar-collapse" id="navbarNav">
+				<ul className="navbar-nav ml-auto">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Dropdown
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Action</a>
+							<a class="dropdown-item" href="#">Another action</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Something else here</a>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<main id="main" className="container">
+			<Provider store={ store }>
+				<ConnectedRouter history = { cfg.history }>
+					<Route exact path="/" component={ Home } />
+				</ConnectedRouter>
+			</Provider>
+		</main>
+	</div>,
+	document.getElementById('root')
+)
